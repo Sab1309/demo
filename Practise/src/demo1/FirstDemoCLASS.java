eafbaebd3575585e78161d67323f71c8227e1222
swati <swati@bhalerao>
2018-06-08 17:42:27 +0530

    first commit

---
+++ Practise/src/demo1/FirstDemoCLASS.java
@@ -0,0 +1,28 @@
+package demo1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstDemoCLASS {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "G:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://toolsqa.com/automation-practice-form/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.partialLinkText("Partial")).click();
+		
+		driver.findElement(By.linkText("Link Test")).click();
+		driver.navigate().back();
+		
+		driver.findElement(By.name("firstname")).sendKeys("swati");
+		driver.findElement(By.name("lastname")).sendKeys("bhalerao");
+		
+		
+
+	}
+
+}
